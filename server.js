eaa2bd512da90aad1856d0bc7fcc5ec37ff539dd
Bhavya Wahie <bhavyawahie@gmail.com>
2021-04-01 23:32:28 +0530

    Addition function added

--- server.js
+++ server.js
@@ -1,51 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get("/",(req, res)=>{ 
-    res.send("<h1>Calculator</h1>");
-});
-// function add(q1, q2){
-//     return q1+q2;
-// }
+app.use(express.urlencoded());
 
-// function sub(q1, q2){
-//     return q1-q2;
-// }
-// function mul(q1, q2){
-//     return q1*q2;
-// }
-// function div(q1, q2){
-//     return q1/q2;
-// }
-// function calculate(q1, q3, q2){
-//     return q3(q1,q2);
-// }
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/",(req, res) =>{
+    let v1 = parseInt(req.body.n1);
+    let v2 = parseInt(req.body.n2);
+    let result = v1 + v2;
+    res.send("Answer = " + result);
+});
 
-
-app.get("/calc/add",(req, res) => {
-    let q1 = parseInt(req.query.q1);
-    let q2 = parseInt(req.query.q2);
-    let result = q1+q2;
-    res.send("Answer " + result);
-});
-app.get("/calc/sub",(req, res) => {
-    let q1 = parseInt(req.query.q1);
-    let q2 = parseInt(req.query.q2);
-    let result = q1-q2;
-    res.send("Answer " + result);
-});
-app.get("/calc/mult",(req, res) => {
-    let q1 = parseInt(req.query.q1);
-    let q2 = parseInt(req.query.q2);
-    let result = q1*q2;
-    res.send("Answer " + result);
-});
-app.get("/calc/div",(req, res) => {
-    let q1 = parseInt(req.query.q1);
-    let q2 = parseInt(req.query.q2);
-    let result = q1/q2;
-    res.send("Answer " + result);
-});
 
 app.listen("3000", (req, res) =>{
     console.log("Server Running at port 3000");
